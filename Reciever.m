c6b7894e481b6c684905f71044e2d3fd0a715a98
Dennis Chen <dennis.chen@students.olin.edu>
2015-12-08 09:24:16 -0500

    changed time vector to be discrete

--- Reciever.m
+++ Reciever.m
@@ -25,19 +25,37 @@ function r = Reciever()
        %to remove phase offset. Operates with assumption
        %that phase offset is not changing significantly over
        %the duration of the signal
-       times = linspace(0,length(signal)/dataRate,length(signal));
+       times = [0:length(signal)-1];
        corrected = signal .* exp(-times*2*pi*1i*freqOffset);
     end
 
-signal = readDATFile('staircase.dat');
+    function stripped = stripZeros(signal)
+       %removes values beneath threshold at beginning and end of 
+       %block
+       THRESHOLD = 50;
+       SIGNAL_START = 100; %start scanning from after the weird spike
+                         %at the beginning of recieved signal
+       i = SIGNAL_START;
+       while signal(i) < THRESHOLD
+           i = i+1;
+       end
+       j = length(signal);
+       while signal(j) < THRESHOLD
+           j = j-1;
+       end
+       stripped = signal(i:j);
+    end
+
+signal = stripZeros(readDATFile('staircase.dat'));
+plot(abs(stripZeros(signal)));
 dataRate = 2.5e5;
 freqOffset = findFreqOffset(signal);
 correctedSignal = removeFreqOffset(signal,dataRate,freqOffset);
-subplot(2,1,1);
-plot(abs(fft(signal.^2)));
-subplot(2,1,2);
-plot(abs(fft(correctedSignal.^2)));
-figure;
-plot(abs(correctedSignal-signal));
+ subplot(2,1,1);
+ plot(real(signal));
+ subplot(2,1,2);
+ plot(real(correctedSignal));
+% figure;
+% plot(abs(correctedSignal-signal));
 
 end
