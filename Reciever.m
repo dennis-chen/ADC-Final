c53447d05e5825b65ac5cc591f951276b8ddd7aa
Dennis Chen <dennis.chen@students.olin.edu>
2015-12-08 00:50:29 -0500

    added time vector to frequency offset calculation, still doesn't seem to work

--- Reciever.m
+++ Reciever.m
@@ -2,12 +2,12 @@ function r = Reciever()
 
     function signal = readDATFile(fileName)
         %reads DAT file produced by rx_samples_to_file, returns
-        %complex signal
+        %complex signal as a row vector
         f1 = fopen(fileName,'r');
         x = fread(f1,'int16');
         real = x(1:2:end);
         imaginary = x(2:2:end);
-        signal = real + imaginary * 1i;
+        signal = (real + imaginary * 1i)';
     end
 
     function freqOffset = findFreqOffset(signal)
@@ -20,17 +20,24 @@ function r = Reciever()
        freqOffset = frequencies(maxFreqIndex)/2;
     end
 
-    function corrected = removeFreqOffset(signal,freqOffset)
+    function corrected = removeFreqOffset(signal,dataRate,freqOffset)
        %multiplies signal by correct complex exponential
        %to remove phase offset. Operates with assumption
        %that phase offset is not changing significantly over
        %the duration of the signal
-       corrected = signal * exp(-1*2*pi*1i*freqOffset);
+       times = linspace(0,length(signal)/dataRate,length(signal));
+       corrected = signal .* exp(-times*2*pi*1i*freqOffset);
     end
 
-signal = readDATFile('rxStaircase.dat');
+signal = readDATFile('staircase.dat');
+dataRate = 2.5e5;
 freqOffset = findFreqOffset(signal);
-correctedSignal = removeFreqOffset(signal,freqOffset);
-plot(abs(correctedSignal));
+correctedSignal = removeFreqOffset(signal,dataRate,freqOffset);
+subplot(2,1,1);
+plot(abs(fft(signal.^2)));
+subplot(2,1,2);
+plot(abs(fft(correctedSignal.^2)));
+figure;
+plot(abs(correctedSignal-signal));
 
 end
