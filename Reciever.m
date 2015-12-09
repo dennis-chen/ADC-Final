4bd96d5abba3eb2413dd5b922c7fcde711b55463
Dennis Chen <dennis.chen@students.olin.edu>
2015-12-09 00:02:06 -0500

    restructured code

--- Reciever.m
+++ Reciever.m
@@ -7,7 +7,42 @@ function r = Reciever()
         x = fread(f1,'int16');
         real = x(1:2:end);
         imaginary = x(2:2:end);
-        signal = (real + imaginary * 1i)';
+        signal = (real + imaginary * 1i);
+    end
+
+    function stripped = stripZeros(signal)
+       %removes values beneath threshold at beginning and end of 
+       %block
+       THRESHOLD = 50;
+       SIGNAL_START = 100; %start scanning from after the weird spike
+                         %at the beginning of recieved signal
+       i = SIGNAL_START;
+       while abs(signal(i)) < THRESHOLD && i < length(signal)
+           i = i+1;
+       end
+       j = length(signal);
+       while abs(signal(j)) < THRESHOLD && j > 0
+           j = j-1;
+       end
+       stripped = signal(i:j);
+    end
+
+    function strippedSignal = getAndStripSignal(fileName)
+        signal = stripZeros(readDATFile(fileName));
+        signalWithoutInitialPeak = signal(2000:length(signal));
+        strippedSignal = stripZeros(signalWithoutInitialPeak);
+    end
+
+    function void = plotComplex(signal)
+       %creates a new figure and plots real and imaginary parts of signal
+       %in subplots
+       figure;
+       subplot(2,1,1);
+       plot(real(signal));
+       xlabel('Real component');
+       subplot(2,1,2);
+       plot(imag(signal));
+       xlabel('Imaginary component');
     end
 
     function freqOffset = findFreqOffset(signal)
@@ -20,59 +55,24 @@ function r = Reciever()
        freqOffset = frequencies(maxFreqIndex)/4;
     end
 
-    function corrected = removeFreqOffset(signal,freqOffset)
+    function [freqOffset, corrected] = removeFreqOffset(signal)
        %multiplies signal by correct complex exponential
        %to remove phase offset. Operates with assumption
        %that phase offset is not changing significantly over
        %the duration of the signal
-       times = 0:length(signal)-1;
+       freqOffset = findFreqOffset(signal);
+       times = (0:length(signal)-1)';
        corrected = signal .* exp(-times*2*pi*1i*freqOffset);
     end
 
-    function stripped = stripZeros(signal)
-       %removes values beneath threshold at beginning and end of 
-       %block
-       THRESHOLD = 50;
-       SIGNAL_START = 100; %start scanning from after the weird spike
-                         %at the beginning of recieved signal
-       i = SIGNAL_START;
-       while signal(i) < THRESHOLD && i < length(signal)
-           i = i+1;
-       end
-       j = length(signal);
-       while signal(j) < THRESHOLD && j > 0
-           j = j-1;
-       end
-       stripped = signal(i:j);
-    end
-
-signal = stripZeros(readDATFile('squareWaveWithImag.dat'));
-%plot(abs(readDATFile('squareWaveWithImag.dat')));
-thingWithoutPeak = signal(2000:length(signal));
-signal = stripZeros(thingWithoutPeak);
-[yI, yQ, freq_offset]  = bpsk_timing_sync(real(signal)', imag(signal)');
-subplot(2,1,1);
-plot(yI);
-subplot(2,1,2);
-plot(yQ);
-freq_offset
-ourFreqOffset = findFreqOffset(signal)
-correctedSignal = removeFreqOffset(signal,ourFreqOffset);
-figure;
-subplot(2,1,1);
-plot(real(correctedSignal));
-subplot(2,1,2);
-plot(imag(correctedSignal));
-
-
-%plot(imag(signal));
-%signal = signal(1:1000);
-% 
-% dataRate = 2.5e5;
-
-% subplot(2,1,1);
-% plot(real(signal));
-% subplot(2,1,2);
-% plot(real(correctedSignal));
+signal = getAndStripSignal('squareWaveWithImag.dat');
+%Siddhartan's timing sync code
+[yI, yQ, siddFreqOffset]  = bpsk_timing_sync(real(signal), imag(signal));
+plotComplex(yI+1j*yQ);
+disp(siddFreqOffset);
+%Our timing sync code
+[freqOffset, correctedSignal] = removeFreqOffset(signal);
+plotComplex(correctedSignal);
+disp(freqOffset);
 
 end
